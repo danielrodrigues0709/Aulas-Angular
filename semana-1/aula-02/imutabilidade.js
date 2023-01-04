6e58a1c7282f14b31f31301703ef6dd5632832b1
Rodrigo Alves <rodrigo.alves@acct.global>
2023-01-04 00:54:20 -0300

    class 2

---
+++ semana-1/aula-02/imutabilidade.js
@@ -0,0 +1,14 @@
+const pedido = { amount: 1000, canceled: false };
+
+const copiaPedido = (pedido) => {
+  // const novoPedido = Object.assign({}, pedido);
+  // novoPedido.canceled = true;
+  pedido.canceled = true;
+  return pedido; // return novoPedido
+};
+
+const novoPedido = copiaPedido(pedido);
+novoPedido.amount = 200
+
+console.log(pedido); // { amount: 200, canceled: true }
+console.log(novoPedido); // { amount: 200, canceled: true }
