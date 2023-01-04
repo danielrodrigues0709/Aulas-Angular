ffad8aee6ad35e46e74825555de3f7d11b6ecf1f
Rodrigo Alves <rodrigo.alves@acct.global>
2023-01-04 16:14:45 -0300

    improve classes

---
+++ semana-1/aula-03/events/events.js
@@ -0,0 +1,29 @@
+const urlUF = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+const cidades = document.getElementById('cidades');
+const uf = document.getElementById('uf');
+
+window.addEventListener('load', async () => {
+  const request = await fetch(urlUF);
+  const response = await request.json();
+
+  const options = document.createElement('optgroup');
+  options.setAttribute('label', 'UFs');
+  response.map(uf => 
+    options.innerHTML += `<option>${uf.sigla}</option>` 
+  );
+
+  uf.append(options);
+});
+
+uf.addEventListener('change', async (event) => {
+  const urlCidades = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf.value}/municipios`;
+  const request = await fetch(urlCidades);
+  const response = await request.json();
+
+  let options = '';
+  response.map(cidade => {
+    options += `<option>${cidade.nome}</option>`
+  });
+
+  cidades.innerHTML += options;
+})
